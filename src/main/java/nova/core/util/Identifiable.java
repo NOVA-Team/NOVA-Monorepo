a82ceff863ffa33815d6005f1405fba855bd3528
RX14 <chris@rx14.co.uk>
2015-01-26 23:38:31 +0000

    Add Identifiable JavaDoc

--- src/main/java/nova/core/util/Identifiable.java
+++ src/main/java/nova/core/util/Identifiable.java
@@ -1,5 +1,14 @@
 package nova.core.util;
 
+/**
+ * A generic interface signifying that this object is identifiable
+ * by an ID
+ */
 public interface Identifiable {
+	/**
+	 * Get the ID to identify this object by
+	 *
+	 * @return the ID
+	 */
 	String getID();
 }
