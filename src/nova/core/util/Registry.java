2bfea552b83a35af607ecaaaa1679f7e69b280f4
RX14 <chris@rx14.co.uk>
2015-01-26 18:47:20 +0000

    Removed list of objects

--- src/nova/core/util/Registry.java
+++ src/nova/core/util/Registry.java
@@ -2,32 +2,29 @@ package nova.core.util;
 
 import com.google.common.collect.HashBiMap;
 
-import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Registry<T extends Named> implements Iterable<T> {
-	private final ArrayList<T> objects = new ArrayList<T>();
-	private final HashBiMap<String, T> names = HashBiMap.create();
+	private final HashBiMap<String, T> objects = HashBiMap.create();
 
 	public void register(T object) {
-		objects.add(object);
-		names.put(object.getName(), object);
+		objects.put(object.getName(), object);
 	}
 
 	public boolean contains(String name) {
-		return names.containsKey(name);
+		return objects.containsKey(name);
 	}
 
 	public T get(String name) {
-		return names.get(name);
+		return objects.get(name);
 	}
 
 	public String getName(T object) {
-		return names.inverse().get(object);
+		return objects.inverse().get(object);
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		return objects.iterator();
+		return objects.values().iterator();
 	}
 }
