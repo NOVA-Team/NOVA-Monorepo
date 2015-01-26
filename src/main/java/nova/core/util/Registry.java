66f7c99dea4863de5681b251da08b8d9775d8493
RX14 <chris@rx14.co.uk>
2015-01-26 22:55:23 +0000

    Registry Doccumentation

--- src/main/java/nova/core/util/Registry.java
+++ src/main/java/nova/core/util/Registry.java
@@ -6,27 +6,57 @@ import java.util.Iterator;
 import java.util.Objects;
 
 /**
- * @param <T>
+ * A registry of one type of identifiable object.
+ *
+ * @param <T> The object type
  */
 public class Registry<T extends Identifiable> implements Iterable<T> {
 	private final HashBiMap<String, T> objects = HashBiMap.create();
 
+	/**
+	 * Registers an identifiable object in the registry
+	 *
+	 * @param object the object to register
+	 */
 	public void register(T object) {
 		objects.put(object.getID(), object);
 	}
 
-	public boolean contains(String name) {
-		return objects.containsKey(name);
+	/**
+	 * Tests whether a given ID is in the Registry
+	 *
+	 * @param ID the id to find
+	 * @return true if the registry contains the object with the given ID
+	 */
+	public boolean contains(String ID) {
+		return objects.containsKey(ID);
 	}
 
-	public T get(String name) {
-		return Objects.requireNonNull(objects.get(name), name + " is not registered");
+	/**
+	 * Gets the object with the given id from the registry
+	 *
+	 * @param ID the id to find
+	 * @return the object found
+	 * @throws java.lang.NullPointerException if the ID doesn't exist in the registry
+	 */
+	public T get(String ID) {
+		return Objects.requireNonNull(objects.get(ID), ID + " is not registered");
 	}
 
+	/**
+	 * Gets the name of a given object
+	 *
+	 * @param object the object to find
+	 * @return the name of the object
+	 * @throws java.lang.NullPointerException if the object doesn't exist in the registry
+	 */
 	public String getName(T object) {
 		return Objects.requireNonNull(objects.inverse().get(object), object.getID() + " is not registered");
 	}
 
+	/**
+	 * @return an iterator on the values of the registry
+	 */
 	@Override
 	public Iterator<T> iterator() {
 		return objects.values().iterator();
