be44050c13e01d78dfe480f100ba65bb3c1f3c41
RX14 <chris@rx14.co.uk>
2015-01-27 23:25:05 +0000

    Clean up Test to use assertj

--- src/test/java/nova/test/di/BasicInjectionTest.java
+++ src/test/java/nova/test/di/BasicInjectionTest.java
@@ -4,8 +4,7 @@ import java.util.Optional;
 
 import nova.bootstrap.DependencyInjectionEntryPoint;
 import nova.core.game.Game;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
@@ -14,10 +13,12 @@ public class BasicInjectionTest {
 	@Test
 	public void starting_injection_should_not_crash(){
 		DependencyInjectionEntryPoint ep = new DependencyInjectionEntryPoint();
+
 		ep.preInit();
 		ep.init(Optional.empty());
 		Game game = ep.postInit(Optional.empty());
-		assertThat(game, notNullValue());
+
+		assertThat(game).isNotNull();
 	}
 	
 }
