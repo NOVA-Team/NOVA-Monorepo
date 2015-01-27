408052ca02f8665a90696a719a4102ce59cece10
RX14 <chris@rx14.co.uk>
2015-01-27 17:26:59 +0000

    Add forEachStoredField in Storabel

--- src/main/java/nova/core/util/components/Storable.java
+++ src/main/java/nova/core/util/components/Storable.java
@@ -4,26 +4,32 @@ import nova.core.util.Stored;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import java.util.function.BiConsumer;
 
 public interface Storable {
 	default void saveToStore(Map<String, Object> data) {
-		for (Field f : this.getClass().getFields()) {
-			if (f.isAnnotationPresent(Stored.class)) {
-				String key = f.getAnnotation(Stored.class).key();
-				if (key.length() == 0) {
-					key = f.getName();
-				}
+		forEachStoredField((field, key) -> {
+			try {
+				data.put(key, field.get(this));
+			} catch (IllegalAccessException e) {
+				// TODO
+			}
+		});
+	}
 
+	default void loadFromStore(Map<String, Object> data) {
+		forEachStoredField((field, key) -> {
+			if (data.containsKey(key)) {
 				try {
-					data.put(key, f.get(this));
+					field.set(this, data.get(key));
 				} catch (IllegalAccessException e) {
 					// TODO
 				}
 			}
-		}
+		});
 	}
 
-	default void loadFromStore(Map<String, Object> data) {
+	default void forEachStoredField(BiConsumer<Field, String> action) {
 		for (Field f : this.getClass().getFields()) {
 			if (f.isAnnotationPresent(Stored.class)) {
 				String key = f.getAnnotation(Stored.class).key();
@@ -31,13 +37,7 @@ public interface Storable {
 					key = f.getName();
 				}
 
-				if (data.containsKey(key)) {
-					try {
-						f.set(this, data.get(key));
-					} catch (IllegalAccessException e) {
-						// TODO
-					}
-				}
+				action.accept(f, key);
 			}
 		}
 	}
