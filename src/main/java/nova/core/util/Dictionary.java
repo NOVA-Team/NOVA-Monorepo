2a4bc886cf11aa1b4742234caa0e4bd3ef07e46b
RX14 <chris@rx14.co.uk>
2015-01-27 22:53:05 +0000

    Document Dictionary

--- src/main/java/nova/core/util/Dictionary.java
+++ src/main/java/nova/core/util/Dictionary.java
@@ -1,18 +1,17 @@
 package nova.core.util;
 
-import com.google.common.collect.HashBiMap;
 import com.google.inject.Inject;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 /**
- * A dictionary of one type of identifiable object.
+ * A dictionary where each identifying string represents a set of objects
+ * and each object can have a set of identifiers.
+ *
  * @param &lt;T&gt; The object type
  */
 public class Dictionary<T> {
@@ -26,6 +25,8 @@ public class Dictionary<T> {
 
 	/**
 	 * Add an object to the dictionary.
+	 *
+	 * @param name the name of the object.
 	 * @param object the object to register.
 	 */
 	public void add(String name, T object) {
@@ -45,8 +46,9 @@ public class Dictionary<T> {
 
 	/**
 	 * Get an object set from the dictionary.
-	 * @param name the dictionary name
-	 * @return the list of objects
+	 *
+	 * @param name the dictionary name.
+	 * @return the list of objects.
 	 */
 	public Set<T> get(String name) {
 		if (!entries.containsKey(name)) {
@@ -56,6 +58,12 @@ public class Dictionary<T> {
 		return Collections.unmodifiableSet(entries.get(name));
 	}
 
+	/**
+	 * Find the names of a given object.
+	 *
+	 * @param object the object to find.
+	 * @return the list of names this object is identified by
+	 */
 	public Set<String> find(T object) {
 		if (!locations.containsKey(object)) {
 			locations.put(object, new HashSet<>());
@@ -64,6 +72,9 @@ public class Dictionary<T> {
 		return Collections.unmodifiableSet(locations.get(object));
 	}
 
+	/**
+	 * @return a {@link java.util.Set Set} of the names in this dictionary.
+	 */
 	public Set<String> keys() {
 		return entries.keySet();
 	}
