b8bf44a2518241c0d91d075dc7db6d7504ed9876
Calclavia <admin@calclavia.com>
2015-01-30 21:44:09 +0800

    Added Vector Scala syntactic sugar methods

--- src/main/java/nova/wrapper/mc1710/backward/render/MinecraftArtist.java
+++ src/main/java/nova/wrapper/mc1710/backward/render/MinecraftArtist.java
@@ -1,9 +1,20 @@
 package nova.wrapper.mc1710.backward.render;
 
+import net.minecraft.client.renderer.Tessellator;
 import nova.core.render.Artist;
 
 /**
  * @author Calclavia
  */
 public class MinecraftArtist extends Artist {
+
+	/**
+	 * Completes this rendering masterpiece.
+	 * @param tesselator - The Minecraft tesselator.
+	 */
+	public void complete(Tessellator tesselator) {
+		tesselator.draw();
+		tesselator.startDrawingQuads();
+		canvases.forEach(c -> c.vertices.forEach(v -> tesselator.addVertex(v.x, v.y, v.z)));
+	}
 }
