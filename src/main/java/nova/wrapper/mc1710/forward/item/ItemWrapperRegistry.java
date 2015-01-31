3de6dd9d1731254c4b7e722f85405a517836e6f0
Calclavia <admin@calclavia.com>
2015-01-31 19:23:24 +0800

    Fixed itemblocks registering twice

--- src/main/java/nova/wrapper/mc1710/forward/item/ItemWrapperRegistry.java
+++ src/main/java/nova/wrapper/mc1710/forward/item/ItemWrapperRegistry.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
 import nova.core.game.Game;
 import nova.core.item.Item;
+import nova.core.item.ItemBlock;
 import nova.wrapper.mc1710.launcher.NovaMinecraft;
 
 import java.util.HashMap;
@@ -30,13 +31,17 @@ public class ItemWrapperRegistry {
 	public void registerItems() {
 		Game.instance.get().itemManager.registry.forEach(itemFactory -> {
 			ItemWrapper itemWrapper = new ItemWrapper(itemFactory);
-			wrapperMap.put(itemFactory.getDummy(), itemWrapper);
-			NovaMinecraft.proxy.registerItem(itemWrapper);
-			GameRegistry.registerItem(itemWrapper, itemFactory.getID());
 
-			//TODO: Testing purposes:
-			itemWrapper.setCreativeTab(CreativeTabs.tabBlock);
-			System.out.println("[NOVA]: Registered '" + itemFactory.getID() + "' item.");
+			//We won't register ItemBlocks twice
+			if (!(itemFactory.getDummy() instanceof ItemBlock)) {
+				wrapperMap.put(itemFactory.getDummy(), itemWrapper);
+				NovaMinecraft.proxy.registerItem(itemWrapper);
+				GameRegistry.registerItem(itemWrapper, itemFactory.getID());
+
+				//TODO: Testing purposes:
+				itemWrapper.setCreativeTab(CreativeTabs.tabBlock);
+				System.out.println("[NOVA]: Registered '" + itemFactory.getID() + "' item.");
+			}
 		});
 	}
 }
