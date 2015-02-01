af86c31edc97236fa0149bc4d3009020ca4a9c7b
Skye Macdonald <skyem@hotmail.co.uk>
2015-02-01 21:11:41 +0000

    Added some javadoc for nova.core.block.components.block.components.Block

--- src/main/java/nova/core/block/Block.java
+++ src/main/java/nova/core/block/Block.java
@@ -23,40 +23,78 @@ public abstract class Block implements Identifiable {
 	private final BlockAccess blockAccess = BlockAccessDummy.INSTANCE;
 	private final Vector3i position = Vector3i.ZERO;
 
+	/**
+	 * Called to get the BlockFactory that refers to this Block class.
+	 * @return The BlockFactory that refers to this Block class
+	 */
 	public final BlockFactory getFactory() {
 		return Game.instance.get().blockManager.getBlockFactory(this.getID()).get();
 	}
 
+	/**
+	 * Get the BlockAccess that refers to this block.
+	 * @return The BlockAccess that refers to this block
+	 */
 	public BlockAccess getBlockAccess() {
 		return blockAccess;
 	}
 
+	/**
+	 * Get the position of the block.
+	 * @return The position of the block
+	 */
 	public Vector3i getPosition() {
 		return position;
 	}
 
+	/**
+	 * Get the x co-ordinate of the block.
+	 * @return The x co-ordinate of the block
+	 */
 	public final int x() {
 		return getPosition().x;
 	}
 
+	/**
+	 * Get the y co-ordinate of the block.
+	 * @return The y co-ordinate of the block
+	 */
 	public final int y() {
 		return getPosition().y;
 	}
 
+	/**
+	 * Get the z co-ordinate of the block.
+	 * @return The z co-ordinate of the block
+	 */
 	public final int z() {
 		return getPosition().z;
 	}
 
+	/**
+	 * Called to get the drops of this block.
+	 * @return A collection of ItemStacks that this block drops
+	 */
 	public Collection<ItemStack> getDrops() {
 		ArrayList<ItemStack> list = new ArrayList<>();
 		list.add(new ItemStack(Game.instance.get().itemManager.getItemFromBlock(this))); // -100 style points.
 		return list;
 	}
 
+	/**
+	 * Called to get the bounding box of this block.
+	 * @return The bounding box of this block
+	 */
 	public Cuboid getBoundingBox() {
 		return new Cuboid(new Vector3i(0, 0, 0), new Vector3i(1, 1, 1));
 	}
 
+	/**
+	 * Called to get the collision boxes for this block.
+	 * @param intersect TODO: No clue what this is
+	 * @param entity The entity that is colliding
+	 * @return TODO: No clue what this is
+	 */
 	public Set<Cuboid> getCollidingBoxes(Cuboid intersect, Optional<Entity> entity) {
 		Set<Cuboid> bounds = new HashSet<>();
 		Cuboid defaultBound = getBoundingBox();
@@ -68,32 +106,51 @@ public abstract class Block implements Identifiable {
 		return bounds;
 	}
 
+	/**
+	 * Called to check if the block is a cube.
+	 * @return True is this block is a cube
+	 */
 	public boolean isCube() {
 		return getBoundingBox().isCube();
 	}
 
+	/**
+	 * Called to check if the block is an opaque cube.
+	 * @return True is this block is a cube that is opaque
+	 */
 	public boolean isOpaqueCube() {
 		return isCube();
 	}
 
+	/**
+	 * Called when a block next to this one changes (removed, placed, etc...).
+	 * @param neighborPosition The position of the block that changed
+	 */
 	public void onNeighborChange(Vector3i neighborPosition) {
 
 	}
-
+	/**
+	 * Called when the block is placed.
+	 * @param changer The BlockChanger that placed the block
+	 */
 	public void onPlaced(BlockChanger changer) {
 
 	}
 
+	/**
+	 * Called when the block is removed.
+	 * @param changer The BlockChanger that removed the block
+	 */
 	public void onRemoved(BlockChanger changer) {
 
 	}
 
 	/**
 	 * Called when the block is left clicked.
-	 * @param entity - The entity that right clicked this object. Most likely a player.
-	 * @param side - The side it was clicked.
-	 * @param hit - The position it was clicked.
-	 * @return True if the right click action does something.
+	 * @param entity The entity that right clicked this object. Most likely a player
+	 * @param side The side it was clicked
+	 * @param hit The position it was clicked
+	 * @return True if the right click action does something
 	 */
 	public boolean onLeftClick(Entity entity, int side, Vector3d hit) {
 		return false;
@@ -101,14 +158,15 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called when the block is right clicked.
-	 * @param entity - The entity that right clicked this object. Most likely a player.
-	 * @param side - The side it was clicked.
-	 * @param hit - The position it was clicked.
-	 * @return True if the right click action does something.
+	 * @param entity The entity that right clicked this object. Most likely a player
+	 * @param side The side it was clicked
+	 * @param hit The position it was clicked
+	 * @return True if the right click action does something
 	 */
 	public boolean onRightClick(Entity entity, int side, Vector3d hit) {
 		return false;
 	}
+
 
 	/**
 	 * Called when an entity collides with this block.
@@ -121,7 +179,7 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called when this block is to be rendered.
-	 * @param model The artist who is rendering this block.
+	 * @param model a {@link nova.core.render.model.Model} to use
 	 */
 	public void renderWorld(Model model) {
 		model.renderBlock(this);
@@ -129,7 +187,7 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called for a dynamic render.
-	 * @param model an {@link nova.core.render.model.Model} to use
+	 * @param model a {@link nova.core.render.model.Model} to use
 	 */
 	public void renderDynamic(Model model) {
 	}
@@ -140,7 +198,7 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called when the item of this block is to be rendered.
-	 * @param model The artist who is rendering this block.
+	 * @param model a {@link nova.core.render.model.Model} to use
 	 */
 	public void renderItem(Model model) {
 		renderWorld(model);
