37a438fdda44cd2f1d4264d0ffdf7e7cfd24cf47
Skye Macdonald <skyem@hotmail.co.uk>
2015-02-01 21:11:41 +0000

    Added some javadoc for nova.core.block.components.block.components.Block

--- src/main/java/nova/core/block/Block.java
+++ src/main/java/nova/core/block/Block.java
@@ -31,28 +31,52 @@ public abstract class Block implements Identifiable {
 		return blockAccess;
 	}
 
+	/**
+	 * Get the position of the block
+	 * @return The position of the block
+	 */
 	public Vector3i getPosition() {
 		return position;
 	}
 
+	/**
+	 * Get the x co-ordinate of the block
+	 * @return The x co-ordinate of the block
+	 */
 	public final int x() {
 		return getPosition().x;
 	}
 
+	/**
+	 * Get the y co-ordinate of the block
+	 * @return The y co-ordinate of the block
+	 */
 	public final int y() {
 		return getPosition().y;
 	}
 
+	/**
+	 * Get the z co-ordinate of the block
+	 * @return The z co-ordinate of the block
+	 */
 	public final int z() {
 		return getPosition().z;
 	}
 
+	/**
+	 * Called to get the drops of this block
+	 * @return A collection of ItemStacks that this block drops
+	 */
 	public Collection<ItemStack> getDrops() {
 		ArrayList<ItemStack> list = new ArrayList<>();
 		list.add(new ItemStack(Game.instance.get().itemManager.getItemFromBlock(this))); // -100 style points.
 		return list;
 	}
 
+	/**
+	 * Called to get the bounding box of this block
+	 * @return The bounding box of this block
+	 */
 	public Cuboid getBoundingBox() {
 		return new Cuboid(new Vector3i(0, 0, 0), new Vector3i(1, 1, 1));
 	}
@@ -68,31 +92,51 @@ public abstract class Block implements Identifiable {
 		return bounds;
 	}
 
+	/**
+	 * Called to check if the block is a cube
+	 * @return True is this block is a cube
+	 */
 	public boolean isCube() {
 		return getBoundingBox().isCube();
 	}
 
+	/**
+	 * Called to check if the block is an opaque cube
+	 * @return True is this block is a cube that is opaque
+	 */
 	public boolean isOpaqueCube() {
 		return isCube();
 	}
 
+	/**
+	 * Called when a neighboring block changes (removed, placed, etc...)
+	 * @param neighborPosition The position of the block that changed
+	 */
 	public void onNeighborChange(Vector3i neighborPosition) {
 
 	}
 
+	/**
+	 * Called when the block is placed.
+	 * @param changer The BlockChanger that placed the block.
+	 */
 	public void onPlaced(BlockChanger changer) {
 
 	}
 
+	/**
+	 * Called when the block is removed.
+	 * @param changer The BlockChanger that removed the block.
+	 */
 	public void onRemoved(BlockChanger changer) {
 
 	}
 
 	/**
 	 * Called when the block is left clicked.
-	 * @param entity - The entity that right clicked this object. Most likely a player.
-	 * @param side - The side it was clicked.
-	 * @param hit - The position it was clicked.
+	 * @param entity The entity that right clicked this object. Most likely a player.
+	 * @param side The side it was clicked.
+	 * @param hit The position it was clicked.
 	 * @return True if the right click action does something.
 	 */
 	public boolean onLeftClick(Entity entity, int side, Vector3d hit) {
@@ -101,9 +145,9 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called when the block is right clicked.
-	 * @param entity - The entity that right clicked this object. Most likely a player.
-	 * @param side - The side it was clicked.
-	 * @param hit - The position it was clicked.
+	 * @param entity The entity that right clicked this object. Most likely a player.
+	 * @param side The side it was clicked.
+	 * @param hit The position it was clicked.
 	 * @return True if the right click action does something.
 	 */
 	public boolean onRightClick(Entity entity, int side, Vector3d hit) {
@@ -121,7 +165,7 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called when this block is to be rendered.
-	 * @param artist The artist who is rendering this block.
+	 * @param artist The artist that is rendering this block.
 	 */
 	public void renderWorld(Artist artist) {
 		artist.renderBlock(this);
@@ -129,18 +173,23 @@ public abstract class Block implements Identifiable {
 
 	/**
 	 * Called for a dynamic render.
-	 * @param artist
+	 * @param artist The artist that is rendering this block.
 	 */
 	public void renderDynamic(Artist artist) {
 	}
 
+	/**
+	 * Called to get the texture of this block for a certain side
+	 * @param side The side of the block that the texture is for
+	 * @return An optional of the texture
+	 */
 	public Optional<Texture> getTexture(Direction side) {
 		return Optional.empty();
 	}
 
 	/**
 	 * Called when the item of this block is to be rendered.
-	 * @param artist The artist who is rendering this block.
+	 * @param artist The artist that is rendering this block.
 	 */
 	public void renderItem(Artist artist) {
 		renderWorld(artist);
