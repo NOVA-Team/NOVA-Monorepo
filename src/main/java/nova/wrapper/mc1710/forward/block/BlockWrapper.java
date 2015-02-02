5af43c187f365bbb0cd0f0e9a1850685969e7b18
Calclavia <admin@calclavia.com>
2015-02-02 13:44:32 +0800

    Fixed tile loading causing crash

--- src/main/java/nova/wrapper/mc1710/forward/block/BlockWrapper.java
+++ src/main/java/nova/wrapper/mc1710/forward/block/BlockWrapper.java
@@ -42,17 +42,15 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.lwjgl.opengl.GL11.GL_TEXTURE_BIT;
-import static org.lwjgl.opengl.GL11.glPopAttrib;
-import static org.lwjgl.opengl.GL11.glPopMatrix;
-import static org.lwjgl.opengl.GL11.glPushAttrib;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * A Minecraft to Nova block wrapper
+ *
  * @author Calclavia
  */
-public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBlockRenderingHandler, IItemRenderer {
+public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBlockRenderingHandler, IItemRenderer
+{
 	public final Block block;
 	/**
 	 * Reference to the wrapped Nova block
@@ -63,7 +61,8 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 	private final int blockRenderingID = RenderingRegistry.getNextAvailableRenderId();
 
 	//TODO: Resolve unknown material issue
-	public BlockWrapper(BlockFactory factory) {
+	public BlockWrapper(BlockFactory factory)
+	{
 		super(Material.piston);
 		this.factory = factory;
 		this.block = factory.getDummy();
@@ -75,93 +74,109 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 		this.lightOpacity = isOpaqueCube() ? 255 : 0;
 	}
 
-	public Block getBlockInstance(net.minecraft.world.IBlockAccess access, Vector3i position) {
+	public Block getBlockInstance(net.minecraft.world.IBlockAccess access, Vector3i position)
+	{
 		/**
 		 * If this block has a TileEntity, forward the method into the Stateful block.
 		 * Otherwise, create a new instance of the block and forward the methods over.
 		 */
-		if (hasTileEntity(0)) {
-			return ((TileWrapper) access.getTileEntity(position.x, position.y, position.z)).block;
-		} else {
-			return getBlockInstance(new BWBlockAccess(access), position);
+		if (hasTileEntity(0))
+		{
+			if (((TileWrapper) access.getTileEntity(position.x, position.y, position.z)).block != null)
+				return ((TileWrapper) access.getTileEntity(position.x, position.y, position.z)).block;
 		}
+		return getBlockInstance(new BWBlockAccess(access), position);
+
 	}
 
-	public Block getBlockInstance(nova.core.block.BlockAccess access, Vector3i position) {
+	public Block getBlockInstance(nova.core.block.BlockAccess access, Vector3i position)
+	{
 		return factory.makeBlock(access, position);
 	}
 
 	@Override
-	public boolean hasTileEntity(int metadata) {
+	public boolean hasTileEntity(int metadata)
+	{
 		//A block requires a TileEntity if it stores data or if it ticks.
 		return Storable.class.isAssignableFrom(blockClass) || Stateful.class.isAssignableFrom(blockClass) || Updater.class.isAssignableFrom(blockClass);
 	}
 
 	@Override
-	public TileEntity createTileEntity(World world, int metadata) {
+	public TileEntity createTileEntity(World world, int metadata)
+	{
 		return new TileWrapper();
 	}
 
 	//TODO: This method seems to only be invoked when a TileEntity changes, not when blocks change!
 	@Override
-	public void onNeighborChange(IBlockAccess access, int x, int y, int z, int tileX, int tileY, int tileZ) {
+	public void onNeighborChange(IBlockAccess access, int x, int y, int z, int tileX, int tileY, int tileZ)
+	{
 		getBlockInstance(access, new Vector3i(x, y, z)).onNeighborChange(new Vector3i(tileX, tileY, tileZ));
 	}
 
 	@Override
-	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack itemStack) {
+	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack itemStack)
+	{
 		getBlockInstance(world, new Vector3i(x, y, z)).onPlaced(new BlockChanger.Entity(BackwardProxyUtil.getEntityWrapper(entity)));
 		//TODO: Should we consider onBlockPlaced also?
 	}
 
 	@Override
-	public void breakBlock(World world, int x, int y, int z, net.minecraft.block.Block block, int i) {
+	public void breakBlock(World world, int x, int y, int z, net.minecraft.block.Block block, int i)
+	{
 		getBlockInstance(world, new Vector3i(x, y, z)).onRemoved(new BlockChanger.Unknown());
 		super.breakBlock(world, x, y, z, block, i);
 	}
 
 	@Override
-	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player) {
+	public void onBlockClicked(World world, int x, int y, int z, EntityPlayer player)
+	{
 		//TODO: Check this raytrace.
 		MovingObjectPosition mop = player.rayTrace(10, 1);
 		getBlockInstance(world, new Vector3i(x, y, z)).onLeftClick(BackwardProxyUtil.getEntityWrapper(player), mop.sideHit, new Vector3d(mop.hitVec.xCoord, mop.hitVec.yCoord, mop.hitVec.zCoord));
 	}
 
 	@Override
-	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ)
+	{
 		return getBlockInstance(world, new Vector3i(x, y, z)).onRightClick(BackwardProxyUtil.getEntityWrapper(player), side, new Vector3d(hitX, hitY, hitZ));
 	}
 
 	@Override
-	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+	public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity)
+	{
 		getBlockInstance(world, new Vector3i(x, y, z)).onEntityCollide(BackwardProxyUtil.getEntityWrapper(entity));
 	}
 
 	@Override
-	public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List list, Entity entity) {
+	public void addCollisionBoxesToList(World world, int x, int y, int z, AxisAlignedBB aabb, List list, Entity entity)
+	{
 		Set<Cuboid> boxes = getBlockInstance(world, new Vector3i(x, y, z))
-			.getCollidingBoxes(new BWCuboid(aabb), entity != null ? Optional.of(BackwardProxyUtil.getEntityWrapper(entity)) : Optional.empty());
+				.getCollidingBoxes(new BWCuboid(aabb), entity != null ? Optional.of(BackwardProxyUtil.getEntityWrapper(entity)) : Optional.empty());
 
 		list.addAll(
-			boxes
-				.stream()
-				.map(c -> c.add(new Vector3i(x, y, z)))
-				.map(CuboidForwardWrapper::new)
-				.collect(Collectors.toList())
+				boxes
+						.stream()
+						.map(c -> c.add(new Vector3i(x, y, z)))
+						.map(CuboidForwardWrapper::new)
+						.collect(Collectors.toList())
 		);
 	}
 
 	@Override
-	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+	public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune)
+	{
 		return getBlockInstance(world, new Vector3i(x, y, z)).getDrops()
-			.stream()
-			.map(WrapUtility::wrapItemStack)
-			.collect(Collectors.toCollection(ArrayList::new));
+				.stream()
+				.map(WrapUtility::wrapItemStack)
+				.collect(Collectors.toCollection(ArrayList::new));
 	}
 
 	@Override
-	public boolean isOpaqueCube() {
-		if (block == null) {
+	public boolean isOpaqueCube()
+	{
+		if (block == null)
+		{
 			// Superconstructor fix. -10 style points.
 			return true;
 		}
@@ -169,20 +184,26 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 	}
 
 	@Override
-	public boolean isNormalCube() {
+	public boolean isNormalCube()
+	{
 		return block.isCube();
 	}
 
 	@Override
-	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z) {
+	public AxisAlignedBB getSelectedBoundingBoxFromPool(World world, int x, int y, int z)
+	{
 		return new CuboidForwardWrapper(getBlockInstance(world, new Vector3i(x, y, z)).getBoundingBox().add(new Vector3i(x, y, z)));
 	}
 
 	@Override
-	public int getLightValue(IBlockAccess access, int x, int y, int z) {
-		if (block instanceof LightEmitter) {
+	public int getLightValue(IBlockAccess access, int x, int y, int z)
+	{
+		if (block instanceof LightEmitter)
+		{
 			return Math.round(((LightEmitter) getBlockInstance(access, new Vector3i(x, y, z))).getEmittedLightLevel() * 15.0F);
-		} else {
+		}
+		else
+		{
 			return 0;
 		}
 	}
@@ -191,13 +212,15 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 	 * Rendering forwarding
 	 */
 	@Override
-	public int getRenderType() {
+	public int getRenderType()
+	{
 		return blockRenderingID;
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public void renderInventoryBlock(net.minecraft.block.Block block, int metadata, int modelId, RenderBlocks renderer) {
+	public void renderInventoryBlock(net.minecraft.block.Block block, int metadata, int modelId, RenderBlocks renderer)
+	{
 		//TODO: We should use the item renderer.
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 		glPushAttrib(GL_TEXTURE_BIT);
@@ -213,7 +236,8 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, net.minecraft.block.Block block, int modelId, RenderBlocks renderer) {
+	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, net.minecraft.block.Block block, int modelId, RenderBlocks renderer)
+	{
 		MinecraftArtist artist = new MinecraftArtist();
 		artist.accessHack = world;
 		this.block.renderWorld(artist);
@@ -224,28 +248,33 @@ public class BlockWrapper extends net.minecraft.block.Block implements ISimpleBl
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public boolean shouldRender3DInInventory(int modelId) {
+	public boolean shouldRender3DInInventory(int modelId)
+	{
 		return true;
 	}
 
 	@SideOnly(Side.CLIENT)
 	@Override
-	public int getRenderId() {
+	public int getRenderId()
+	{
 		return blockRenderingID;
 	}
 
 	@Override
-	public boolean handleRenderType(ItemStack item, ItemRenderType type) {
+	public boolean handleRenderType(ItemStack item, ItemRenderType type)
+	{
 		return true;
 	}
 
 	@Override
-	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper) {
+	public boolean shouldUseRenderHelper(ItemRenderType type, ItemStack item, ItemRendererHelper helper)
+	{
 		return true;
 	}
 
 	@Override
-	public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
+	public void renderItem(ItemRenderType type, ItemStack item, Object... data)
+	{
 		GL11.glPushMatrix();
 		MinecraftArtist artist = new MinecraftArtist();
 		this.block.renderItem(artist);
--- src/main/java/nova/wrapper/mc1710/forward/block/TileWrapper.java
+++ src/main/java/nova/wrapper/mc1710/forward/block/TileWrapper.java
@@ -4,10 +4,9 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import nova.core.block.Block;
 import nova.core.block.components.Stateful;
+import nova.core.game.Game;
 import nova.core.util.components.Storable;
 import nova.core.util.components.Updater;
-import nova.core.util.transform.Vector3i;
-import nova.wrapper.mc1710.backward.world.BWWorld;
 import nova.wrapper.mc1710.util.NBTUtility;
 
 import java.util.HashMap;
@@ -15,34 +14,42 @@ import java.util.Map;
 
 /**
  * A Minecraft TileEntity to Nova block wrapper
+ *
  * @author Calclavia
  */
-public class TileWrapper extends TileEntity {
+public class TileWrapper extends TileEntity
+{
 	public Block block;
 
 	@Override
-	public void validate() {
-		super.validate();
-		block = ((BlockWrapper) getBlockType()).getBlockInstance(new BWWorld(worldObj), new Vector3i(xCoord, yCoord, zCoord));
-		if (block instanceof Stateful) {
+	public void validate()
+	{
+		super.validate();/*
+		block = ((BlockWrapper) worldObj.getChunkFromBlockCoords(xCoord, zCoord).getBlock(xCoord, yCoord, zCoord)).getBlockInstance(new BWWorld(worldObj), new Vector3i(xCoord, yCoord, zCoord));
+		if (block instanceof Stateful)
+		{
 			// TODO: Initialize, by spec, is only called the first time a block is placed.
 			// Perhaps the spec should be changed?
 			((Stateful) block).initialize();
 			((Stateful) block).load();
-		}
+		}*/
 	}
 
 	@Override
-	public void invalidate() {
+	public void invalidate()
+	{
 		super.invalidate();
-		if (block instanceof Stateful) {
+		if (block instanceof Stateful)
+		{
 			((Stateful) block).unload();
 		}
 	}
 
 	@Override
-	public boolean isInvalid() {
-		if (block instanceof Stateful && !((Stateful) block).isValid()) {
+	public boolean isInvalid()
+	{
+		if (block instanceof Stateful && !((Stateful) block).isValid())
+		{
 			return true;
 		}
 		return super.isInvalid();
@@ -52,36 +59,48 @@ public class TileWrapper extends TileEntity {
 	 * Updates the block.
 	 */
 	@Override
-	public void updateEntity() {
+	public void updateEntity()
+	{
 		((Updater) block).update(0.05);
 	}
 
 	/**
 	 * Only register tile updates if the block is an instance of Updater.
+	 *
 	 * @return Whether can update
 	 */
 	@Override
-	public boolean canUpdate() {
+	public boolean canUpdate()
+	{
 		return block instanceof Updater;
 	}
 
 	@Override
-	public void writeToNBT(NBTTagCompound nbt) {
+	public void writeToNBT(NBTTagCompound nbt)
+	{
 		super.writeToNBT(nbt);
 
-		if (block instanceof Storable) {
+		if (block instanceof Storable)
+		{
 			Map<String, Object> data = new HashMap<>();
 			((Storable) block).save(data);
 			nbt.setTag("nova", NBTUtility.mapToNBT(data));
 		}
+
+		if (block != null)
+			nbt.setString("novaID", block.getID());
 	}
 
 	@Override
-	public void readFromNBT(NBTTagCompound nbt) {
+	public void readFromNBT(NBTTagCompound nbt)
+	{
 		super.readFromNBT(nbt);
 
-		if (block instanceof Storable) {
+		if (block instanceof Storable)
+		{
 			((Storable) block).load(NBTUtility.nbtToMap(nbt.getCompoundTag("nova")));
 		}
+
+		block = Game.instance.get().blockManager.getBlock(nbt.getString("novaID")).orElse(null);
 	}
 }
