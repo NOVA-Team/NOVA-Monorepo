3a549b8fe056a234bc4ff1947002cde7aa4af5ac
Skye Macdonald <skyem@hotmail.co.uk>
2015-02-03 20:59:51 +0000

    Did some guesswork for JavaDoc in nova.core.block.components

--- src/main/java/nova/core/block/components/Connectable.java
+++ src/main/java/nova/core/block/components/Connectable.java
@@ -3,6 +3,14 @@ package nova.core.block.components;
 import nova.core.util.Direction;
 
 public interface Connectable {
+
+	/**
+	 * Called to check if a block can connect to another block next to it.
+	 *
+	 * @param type The type of the connectable object
+	 * @param side The side the side the object is connected on
+	 * @return true if this type can be connected
+	 */
 	Connectable.Type canConnect(Class<?> type, Direction side);
 
 	public enum Type {
--- src/main/java/nova/core/block/components/LightEmitter.java
+++ src/main/java/nova/core/block/components/LightEmitter.java
@@ -1,5 +1,10 @@
 package nova.core.block.components;
 
 public interface LightEmitter {
+	/**
+	 * Called to get the amount of light emitted from a block.
+	 *
+	 * @return The level of light that is emitted by the block.
+	 */
 	public float getEmittedLightLevel();
 }
