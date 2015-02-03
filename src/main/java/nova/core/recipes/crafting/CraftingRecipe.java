1bf9d1a9ec0f0cb78eb0784691016c9bac004e58
Victorious3 <Minefoto0@gmail.com>
2015-02-03 18:59:30 +0100

    Small cleanup.

--- src/main/java/nova/core/recipes/crafting/CraftingRecipe.java
+++ src/main/java/nova/core/recipes/crafting/CraftingRecipe.java
@@ -1,11 +1,10 @@
 package nova.core.recipes.crafting;
 
+import java.util.Collection;
+import java.util.Optional;
+
 import nova.core.item.ItemStack;
 import nova.core.recipes.Recipe;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Contains a single crafting recipe, no matter its crafting logic.
@@ -13,44 +12,48 @@ import java.util.Optional;
  * @author Stan Hebben
  */
 public interface CraftingRecipe extends Recipe {
-    /**
-     * Checks if this crafting recipe matches the content of the given crafting grid.
-     *
-     * @param craftingGrid crafting grid to read from
-     * @return true if a crafting operation would return a valid result
-     */
-    public boolean matches(CraftingGrid craftingGrid);
+	/**
+	 * Checks if this crafting recipe matches the content of the given crafting
+	 * grid.
+	 *
+	 * @param craftingGrid crafting grid to read from
+	 * @return true if a crafting operation would return a valid result
+	 */
+	public boolean matches(CraftingGrid craftingGrid);
 
-    /**
-     * Calculates the crafting result for the given crafting grid. Does not modify the contents of the crafting grid.
-     *
-     * @param craftingGrid crafting grid
-     * @return crafting result, empty if the recipe doesn't match
-     */
-    public Optional<ItemStack> getCraftingResult(CraftingGrid craftingGrid);
+	/**
+	 * Calculates the crafting result for the given crafting grid. Does not
+	 * modify the contents of the crafting grid.
+	 *
+	 * @param craftingGrid crafting grid
+	 * @return crafting result, empty if the recipe doesn't match
+	 */
+	public Optional<ItemStack> getCraftingResult(CraftingGrid craftingGrid);
 
-    /**
-     * Gets a nominal (example) output for this recipe. Used in recipe display.
-     *
-     * @return example output
-     */
-    public Optional<ItemStack> getNominalOutput();
+	/**
+	 * Gets a nominal (example) output for this recipe. Used in recipe display.
+	 *
+	 * @return example output
+	 */
+	public Optional<ItemStack> getNominalOutput();
 
-    /**
-     * Consumes items for the crafting of a single item. Removes items and applies any necessary modifications on the
-     * stacks in the given crafting grid, and gives back any items if necessary.
-     *
-     * @param craftingGrid crafting grid to modify
-     */
-    public void consumeItems(CraftingGrid craftingGrid);
+	/**
+	 * Consumes items for the crafting of a single item. Removes items and
+	 * applies any necessary modifications on the stacks in the given crafting
+	 * grid, and gives back any items if necessary.
+	 *
+	 * @param craftingGrid crafting grid to modify
+	 */
+	public void consumeItems(CraftingGrid craftingGrid);
 
-    /**
-     * Gets the possible items for this recipe in the first non-empty recipe slot. Used to index recipes and can return
-     * empty if the recipe is calculated dynamically.
-     *
-     * @return The items
-     */
-    public default Optional<Collection<String>> getPossibleItemsInFirstSlot() {
-        return Optional.empty();
-    }
+	/**
+	 * Gets the possible items for this recipe in the first non-empty recipe
+	 * slot. Used to index recipes and can return empty if the recipe is
+	 * calculated dynamically.
+	 *
+	 * @return The items
+	 */
+	public default Optional<Collection<String>> getPossibleItemsInFirstSlot() {
+		return Optional.empty();
+	}
 }
--- src/main/java/nova/core/recipes/crafting/CraftingRecipeManager.java
+++ src/main/java/nova/core/recipes/crafting/CraftingRecipeManager.java
@@ -1,105 +1,107 @@
 package nova.core.recipes.crafting;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-import nova.core.item.ItemStack;
-import nova.core.recipes.RecipeAddedEvent;
-import nova.core.recipes.RecipeManager;
-import nova.core.recipes.RecipeRemovedEvent;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import nova.core.item.ItemStack;
+import nova.core.recipes.RecipeAddedEvent;
+import nova.core.recipes.RecipeManager;
+import nova.core.recipes.RecipeRemovedEvent;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
 /**
- * Manages crafting recipes and has functions to efficiently lookup a crafting recipe.
+ * Manages crafting recipes and has functions to efficiently lookup a crafting
+ * recipe.
  *
  * @author Stan Hebben
  */
 public class CraftingRecipeManager {
-    private final RecipeManager recipeManager;
-    private final List<CraftingRecipe> dynamicRecipes;
-    private final Multimap<String, CraftingRecipe> staticRecipes;
+	private final RecipeManager recipeManager;
+	private final List<CraftingRecipe> dynamicRecipes;
+	private final Multimap<String, CraftingRecipe> staticRecipes;
 
-    public CraftingRecipeManager(RecipeManager recipeManager) {
-        this.recipeManager = recipeManager;
-        this.dynamicRecipes = new ArrayList<>();
-        this.staticRecipes = ArrayListMultimap.create();
+	public CraftingRecipeManager(RecipeManager recipeManager) {
+		this.recipeManager = recipeManager;
+		this.dynamicRecipes = new ArrayList<>();
+		this.staticRecipes = ArrayListMultimap.create();
 
-        recipeManager.addRecipeAddedListener(CraftingRecipe.class, this::onCraftingRecipeAdded);
-        recipeManager.addRecipeRemovedListener(CraftingRecipe.class, this::onCraftingRecipeRemoved);
-    }
+		recipeManager.addRecipeAddedListener(CraftingRecipe.class, this::onCraftingRecipeAdded);
+		recipeManager.addRecipeRemovedListener(CraftingRecipe.class, this::onCraftingRecipeRemoved);
+	}
 
-    /**
-     * Adds a recipe. Adds it to the global recipe list as CraftingRecipe.
-     *
-     * @param recipe {@link CraftingRecipe}
-     */
-    public void addRecipe(CraftingRecipe recipe) {
-        recipeManager.addRecipe(recipe);
-    }
+	/**
+	 * Adds a recipe. Adds it to the global recipe list as CraftingRecipe.
+	 *
+	 * @param recipe {@link CraftingRecipe}
+	 */
+	public void addRecipe(CraftingRecipe recipe) {
+		recipeManager.addRecipe(recipe);
+	}
 
-    /**
-     * Removes a recipes. Removes if from the global recipe list.
-     *
-     * @param recipe {@link CraftingRecipe}
-     */
-    public void removeRecipe(CraftingRecipe recipe) {
-        recipeManager.removeRecipe(recipe);
-    }
+	/**
+	 * Removes a recipes. Removes if from the global recipe list.
+	 *
+	 * @param recipe {@link CraftingRecipe}
+	 */
+	public void removeRecipe(CraftingRecipe recipe) {
+		recipeManager.removeRecipe(recipe);
+	}
 
-    /**
-     * Gets the recipe that matches the given crafting grid.
-     *
-     * @param grid crafting grid
-     * @return matching crafting recipe, if any
-     */
-    public Optional<CraftingRecipe> getRecipe(CraftingGrid grid) {
-        for (CraftingRecipe dynamicRecipe : dynamicRecipes) {
-            if (dynamicRecipe.matches(grid))
-                return Optional.of(dynamicRecipe);
-        }
+	/**
+	 * Gets the recipe that matches the given crafting grid.
+	 *
+	 * @param grid crafting grid
+	 * @return matching crafting recipe, if any
+	 */
+	public Optional<CraftingRecipe> getRecipe(CraftingGrid grid) {
+		for (CraftingRecipe dynamicRecipe : dynamicRecipes) {
+			if (dynamicRecipe.matches(grid))
+				return Optional.of(dynamicRecipe);
+		}
 
-        Optional<ItemStack> firstItem = grid.getFirstNonEmptyItem();
-        if (!firstItem.isPresent())
-            return Optional.empty();
+		Optional<ItemStack> firstItem = grid.getFirstNonEmptyItem();
+		if (!firstItem.isPresent())
+			return Optional.empty();
 
-        String firstItemId = firstItem.get().getItem().getID();
-        if (!staticRecipes.containsKey(firstItemId))
-            return Optional.empty();
+		String firstItemId = firstItem.get().getItem().getID();
+		if (!staticRecipes.containsKey(firstItemId))
+			return Optional.empty();
 
-        for (CraftingRecipe staticRecipe : staticRecipes.get(firstItemId)) {
-            if (staticRecipe.matches(grid))
-                return Optional.of(staticRecipe);
-        }
+		for (CraftingRecipe staticRecipe : staticRecipes.get(firstItemId)) {
+			if (staticRecipe.matches(grid))
+				return Optional.of(staticRecipe);
+		}
 
-        return Optional.empty();
-    }
+		return Optional.empty();
+	}
 
-    // #######################
-    // ### Private Methods ###
-    // #######################
+	// #######################
+	// ### Private Methods ###
+	// #######################
 
-    private void onCraftingRecipeAdded(RecipeAddedEvent<CraftingRecipe> e) {
-        Optional<Collection<String>> possibleFirstItemIds = e.getRecipe().getPossibleItemsInFirstSlot();
-        if (possibleFirstItemIds.isPresent()) {
-            for (String itemId : possibleFirstItemIds.get()) {
-                staticRecipes.put(itemId, e.getRecipe());
-            }
-        } else {
-            dynamicRecipes.add(e.getRecipe());
-        }
-    }
+	private <T extends CraftingRecipe> void onCraftingRecipeAdded(RecipeAddedEvent<T> e) {
+		Optional<Collection<String>> possibleFirstItemIds = e.getRecipe().getPossibleItemsInFirstSlot();
+		if (possibleFirstItemIds.isPresent()) {
+			for (String itemId : possibleFirstItemIds.get()) {
+				staticRecipes.put(itemId, e.getRecipe());
+			}
+		} else {
+			dynamicRecipes.add(e.getRecipe());
+		}
+	}
 
-    private void onCraftingRecipeRemoved(RecipeRemovedEvent<CraftingRecipe> e) {
-        Optional<Collection<String>> possibleFirstItemIds = e.getRecipe().getPossibleItemsInFirstSlot();
-        if (possibleFirstItemIds.isPresent()) {
-            for (String itemId : possibleFirstItemIds.get()) {
-                staticRecipes.remove(itemId, e.getRecipe());
-            }
-        } else {
-            dynamicRecipes.remove(e.getRecipe());
-        }
-    }
+	private <T extends CraftingRecipe> void onCraftingRecipeRemoved(RecipeRemovedEvent<T> e) {
+		Optional<Collection<String>> possibleFirstItemIds = e.getRecipe().getPossibleItemsInFirstSlot();
+		if (possibleFirstItemIds.isPresent()) {
+			for (String itemId : possibleFirstItemIds.get()) {
+				staticRecipes.remove(itemId, e.getRecipe());
+			}
+		} else {
+			dynamicRecipes.remove(e.getRecipe());
+		}
+	}
 }
