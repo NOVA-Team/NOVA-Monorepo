387a82407ab4d0b8fe8d1faafac4415421787466
Calclavia <admin@calclavia.com>
2015-02-07 19:13:06 +0800

    Added getTooltips() method to Item

--- src/main/java/nova/core/item/Item.java
+++ src/main/java/nova/core/item/Item.java
@@ -2,6 +2,7 @@ package nova.core.item;
 
 import nova.core.entity.Entity;
 import nova.core.inventory.Inventory;
+import nova.core.player.Player;
 import nova.core.render.texture.ItemTexture;
 import nova.core.util.Direction;
 import nova.core.util.Identifiable;
@@ -9,19 +10,27 @@ import nova.core.util.transform.Vector3d;
 import nova.core.util.transform.Vector3i;
 import nova.core.world.World;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Optional;
 
 /**
  * Item that can be stacked using {@link ItemStack}
- *
  * @see ItemStack
  * @see Inventory
  */
 public abstract class Item implements Identifiable {
 
 	/**
+	 * Gets a list of tooltips for this item.
+	 * @return The tooltip strings
+	 */
+	public Collection<String> getTooltips(Optional<Player> player) {
+		return new HashSet<>();
+	}
+
+	/**
 	 * Called when the entity right clicks the item onto the block.
-	 *
 	 * @param entity - The entity using the item
 	 * @param world - The world of the block
 	 * @param position - The position of the block
@@ -35,7 +44,6 @@ public abstract class Item implements Identifiable {
 
 	/**
 	 * Called when an entity right clicks the item.
-	 *
 	 * @param entity - The entity right clicking.
 	 */
 	public void onRightClick(Entity entity) {
@@ -44,7 +52,6 @@ public abstract class Item implements Identifiable {
 
 	/**
 	 * Called when this item is being rendered.
-	 *
 	 * @param type Type
 	 * @param data Data
 	 */
@@ -54,7 +61,6 @@ public abstract class Item implements Identifiable {
 
 	/**
 	 * Gets the texture of this item. If there is no texture provided, it will not render any and default to onRender() method for custom item rendering.
-	 *
 	 * @return {@link ItemTexture} instance
 	 */
 	public Optional<ItemTexture> getTexture() {
