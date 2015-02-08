f9e480282e2e4863171964f81464b1696bb563f6
Calclavia <admin@calclavia.com>
2015-02-08 14:07:45 +0800

    Made InventoryProvider not extend Connectable

--- src/main/java/nova/core/inventory/components/InventoryProvider.java
+++ src/main/java/nova/core/inventory/components/InventoryProvider.java
@@ -1,11 +1,10 @@
 package nova.core.inventory.components;
 
-import nova.core.block.components.Connectable;
 import nova.core.inventory.Inventory;
 import nova.core.util.Direction;
 
 import java.util.Optional;
 
-public interface InventoryProvider extends Connectable {
+public interface InventoryProvider {
 	Optional<Inventory> getInventory(Direction side);
 }
