1914139c1c5fc113a2565fc1bb82a02f99bb7d0a
Calclavia <admin@calclavia.com>
2015-02-15 19:14:02 +0800

    Added serverStarting and serverStopping events

--- src/main/java/nova/wrapper/mc1710/launcher/FMLEventHandler.java
+++ src/main/java/nova/wrapper/mc1710/launcher/FMLEventHandler.java
@@ -1,15 +1,27 @@
 package nova.wrapper.mc1710.launcher;
 
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import nova.core.event.EventManager;
 import nova.internal.tick.UpdateTicker;
 
 /**
  * Handles FML events and forwards them to NOVA.
- *
  * @author Calclavia
  */
 public class FMLEventHandler {
+
+	@SubscribeEvent
+	public void serverStarting(FMLServerStartingEvent event) {
+		EventManager.instance.serverStarting.publish(null);
+	}
+
+	@SubscribeEvent
+	public void serverStopping(FMLServerStoppingEvent event) {
+		EventManager.instance.serverStopping.publish(null);
+	}
 
 	@SubscribeEvent
 	public void tickEnd(TickEvent.ServerTickEvent event) {
