ddb61d5789c0590e41da3f40dd5f58e8c91ac1f6
Victorious3 <Minefoto0@gmail.com>
2015-02-20 11:16:35 +0100

    Use Objects.hash for generating the hashcode of FormattedText

--- src/main/java/nova/core/gui/render/FormattedText.java
+++ src/main/java/nova/core/gui/render/FormattedText.java
@@ -1,7 +1,10 @@
 package nova.core.gui.render;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Consumer;
+
+import nova.core.render.Color;
 
 /**
  * An object that holds text and the format to apply to it. A
@@ -68,7 +71,7 @@ public class FormattedText implements Iterable<FormattedText> {
 		public boolean underline;
 		public boolean strikethrough;
 
-		public int color = 0xFFFFFF;
+		public Color color = Color.white;
 
 		public TextFormat() {
 
@@ -90,15 +93,7 @@ public class FormattedText implements Iterable<FormattedText> {
 
 		@Override
 		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + (bold ? 1231 : 1237);
-			result = prime * result + color;
-			result = prime * result + (italic ? 1231 : 1237);
-			result = prime * result + (shadow ? 1231 : 1237);
-			result = prime * result + (strikethrough ? 1231 : 1237);
-			result = prime * result + (underline ? 1231 : 1237);
-			return result;
+			return Objects.hash(shadow, italic, bold, underline, strikethrough, color);
 		}
 
 		@Override
