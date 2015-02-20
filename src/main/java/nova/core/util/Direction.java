291bb4504570bbd499a3cb378ba213f1f537b40e
Calclavia <admin@calclavia.com>
2015-02-20 13:20:50 +0800

    Added Quaternion rotation value for each Direction

--- src/main/java/nova/core/util/Direction.java
+++ src/main/java/nova/core/util/Direction.java
@@ -1,31 +1,35 @@
 package nova.core.util;
 
+import nova.core.util.transform.Quaternion;
+import nova.core.util.transform.Vector3d;
 import nova.core.util.transform.Vector3i;
 
 /**
  * Defines basic directions in world.
  */
 public enum Direction {
-	DOWN(0, -1, 0),
-	UP(0, 1, 0),
-	NORTH(0, 0, -1),
-	SOUTH(0, 0, 1),
-	WEST(-1, 0, 0),
-	EAST(1, 0, 0),
-	UNKNOWN(0, 0, 0);
+	DOWN(0, -1, 0, Quaternion.fromAxis(Vector3d.xAxis, -90)),
+	UP(0, 1, 0, Quaternion.fromAxis(Vector3d.xAxis, 90)),
+	NORTH(0, 0, -1, Quaternion.fromAxis(Vector3d.yAxis, 90)),
+	SOUTH(0, 0, 1, Quaternion.fromAxis(Vector3d.yAxis, -90)),
+	WEST(-1, 0, 0, Quaternion.fromAxis(Vector3d.yAxis, -180)),
+	EAST(1, 0, 0, Quaternion.fromAxis(Vector3d.yAxis, 0)),
+	UNKNOWN(0, 0, 0, Quaternion.identity);
 
 	public static final Direction[] DIRECTIONS = new Direction[] {
 		DOWN, UP, NORTH, SOUTH, WEST, EAST
 	};
 	private static final Direction[] values = Direction.values();
 	public final int x, y, z;
+	public final Quaternion rotation;
 	private final Vector3i vector;
 
-	Direction(int ox, int oy, int oz) {
-		x = ox;
-		y = oy;
-		z = oz;
-		vector = new Vector3i(x, y, z);
+	Direction(int x, int y, int z, Quaternion rotation) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.vector = new Vector3i(x, y, z);
+		this.rotation = rotation;
 	}
 
 	/**
