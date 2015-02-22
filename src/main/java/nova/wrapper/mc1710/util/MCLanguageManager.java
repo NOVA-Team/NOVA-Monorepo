eff14819b5505c2cbb3b7e2d481bb65aeb3e4ed0
Calclavia <admin@calclavia.com>
2015-02-22 18:31:59 +0800

    Added LanguageManager

---
+++ src/main/java/nova/wrapper/mc1710/util/MCLanguageManager.java
@@ -0,0 +1,15 @@
+package nova.wrapper.mc1710.util;
+
+import net.minecraft.util.StatCollector;
+import nova.core.util.LanguageManager;
+
+/**
+ * @author Calclavia
+ */
+public class MCLanguageManager extends LanguageManager {
+
+	@Override
+	public String getLocal(String key) {
+		return StatCollector.translateToLocal(key);
+	}
+}
