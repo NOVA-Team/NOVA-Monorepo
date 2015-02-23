6a0309fed424728d09d50a3284e8ab24fbb6c62e
Victorious3 <Minefoto0@gmail.com>
2015-02-23 16:16:16 +0100

    Add parser for FormattedText.

--- src/main/java/nova/core/gui/render/FormattedText.java
+++ src/main/java/nova/core/gui/render/FormattedText.java
@@ -3,6 +3,8 @@ package nova.core.gui.render;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import nova.core.render.Color;
 
@@ -19,13 +21,17 @@ public class FormattedText implements Iterable<FormattedText> {
 	private TextFormat format = new TextFormat();
 	private String text;
 
+	public FormattedText() {
+		this.text = "";
+	}
+
 	public FormattedText(String text) {
 		this.text = text;
 	}
 
 	public FormattedText(String text, TextFormat format) {
 		this.text = text;
-		this.format = format;
+		this.format = format.clone();
 	}
 
 	public FormattedText add(FormattedText other) {
@@ -52,8 +58,9 @@ public class FormattedText implements Iterable<FormattedText> {
 			this.text += text;
 			return this;
 		} else {
-			FormattedText child = new FormattedText(text, format);
+			FormattedText child = new FormattedText(text);
 			this.child = child;
+			child.format = format;
 			child.parent = this;
 			return child;
 		}
@@ -64,6 +71,111 @@ public class FormattedText implements Iterable<FormattedText> {
 	}
 
 	public String getText() {
+		return text;
+	}
+
+	public static Pattern pattern = Pattern.compile("(?<!\\)(?:\\\\)*&([^;]{2});|&cr([-+]?\\d+);");
+
+	// TODO untested
+	/**
+	 * <p>
+	 * Parses an arbitrary String to {@link FormattedText}. The format applied
+	 * has the following syntax: {@code &<tag>[data];} There are two types of
+	 * tags, tags without additional data are flags, i.e they are toggled each
+	 * time. {@code &rt;} turns all flags back to {@code false}. Tags with
+	 * additional data (color only currently) are not affected by {@code &rt;}.
+	 * Tags can be escaped with an {@code \} character. The case is taken into
+	 * account, only lowercase characters are permitted for tags.
+	 * </p>
+	 * 
+	 * <p>
+	 * <b>The formatting codes applied are as follows:</b>
+	 * </p>
+	 * 
+	 * <table>
+	 * <tr>
+	 * <th>Code:</th>
+	 * <th>Description:</th>
+	 * <th>Example:</th>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &sh;}</td>
+	 * <td>Toggles the flag for Text shadow</td>
+	 * <td style='text-shadow:black 2px 2px 2px;'>
+	 * "The quick brown fox jumps over the lazy dog"</td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &it;}</td>
+	 * <td>Toggles the flag for italics</td>
+	 * <td><i>"The quick brown fox jumps over the lazy dog"</i></td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &bd;}</td>
+	 * <td>Toggles the flag for bold</td>
+	 * <td><b>"The quick brown fox jumps over the lazy dog"</b></td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &ul;}</td>
+	 * <td>Toggles the flag for underline</td>
+	 * <td><u>"The quick brown fox jumps over the lazy dog"</u></td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &st;}</td>
+	 * <td>Toggles the flag for strikethrough</td>
+	 * <td><s>"The quick brown fox jumps over the lazy dog"</s></td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &rt;}</td>
+	 * <td>Resets all previous flags to {@code false}</td>
+	 * <td>"The quick brown fox jumps over the lazy dog"</td>
+	 * </tr>
+	 * <tr>
+	 * <td>{@code &cr[ARGB];}</td>
+	 * <td>Sets the text color to the given color, you can use
+	 * {@link Color#argb()}.</td>
+	 * <td style='color: blue;'>
+	 * "The quick brown fox jumps over the lazy dog"</td>
+	 * </tr>
+	 * </table>
+	 * 
+	 * @param string String to parse
+	 * @return FormattedText instance
+	 */
+	public static FormattedText parse(String string) {
+		Matcher matcher = pattern.matcher(string);
+		TextFormat format = new TextFormat();
+		FormattedText text = new FormattedText();
+		int index = 0;
+		while (matcher.find()) {
+			int end = matcher.end();
+			int start = matcher.start(1);
+			if (matcher.group(2) != null) {
+				format = format.clone();
+				switch (matcher.group(2)) {
+					case "sh":
+						format.shadow = !format.shadow;
+					case "it":
+						format.italic = !format.italic;
+					case "bd":
+						format.bold = !format.bold;
+					case "ul":
+						format.underline = !format.underline;
+					case "st":
+						format.strikethrough = !format.strikethrough;
+					case "rt":
+						format.reset();
+				}
+			} else {
+				format.color = Color.argb(Integer.parseInt(matcher.group(3)));
+			}
+			if (start - index > 0) {
+				String substr = string.substring(index, start);
+				FormattedText append = new FormattedText(substr);
+				append.format = format;
+				text = text.add(append);
+			}
+			index = end;
+		}
 		return text;
 	}
 
@@ -116,7 +228,14 @@ public class FormattedText implements Iterable<FormattedText> {
 				&& this.shadow == other.shadow 
 				&& this.strikethrough == other.strikethrough 
 				&& this.underline == other.underline 
-				&& this.color == other.color;
+				&& this.color.equals(other.color);
+		}
+
+		/**
+		 * Resets all formatting, the color stays the same.
+		 */
+		public void reset() {
+			shadow = italic = bold = underline = strikethrough = false;
 		}
 	}
 
