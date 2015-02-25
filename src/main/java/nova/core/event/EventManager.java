52864f4ac192e5903dde2ef38e96d61d83a6ff91
Calclavia <admin@calclavia.com>
2015-02-25 11:20:24 +0800

    BlockChangeEvent now receives its previous and new block states

--- src/main/java/nova/core/event/EventManager.java
+++ src/main/java/nova/core/event/EventManager.java
@@ -2,10 +2,10 @@ package nova.core.event;
 
 import nova.core.block.Block;
 import nova.core.util.transform.Vector3i;
+import nova.core.world.World;
 
 /**
  * General event manager that handles basic events
- *
  * @author Calclavia
  */
 public class EventManager {
@@ -25,13 +25,19 @@ public class EventManager {
 	public EventBus<BlockChangeEvent> blockChange = new EventBus<>();
 
 	public static class BlockChangeEvent {
-		//The block that was changed
-		public final Block block;
+		//The world
+		public final World world;
 		//The position of the block
 		public final Vector3i position;
+		//The block that was in this position previously
+		public final Block oldBlock;
+		//The block that was set to in this position
+		public final Block newBlock;
 
-		public BlockChangeEvent(Block block, Vector3i position) {
-			this.block = block;
+		public BlockChangeEvent(World world, Vector3i position, Block oldBlock, Block newBlock) {
+			this.world = world;
+			this.newBlock = newBlock;
+			this.oldBlock = oldBlock;
 			this.position = position;
 		}
 	}
