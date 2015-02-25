556115a1d9f1ed5178b56901b5c00cb1c571bf1a
Calclavia <admin@calclavia.com>
2015-02-25 14:44:45 +0800

    Players now implement Damageable

--- src/main/java/nova/wrapper/mc1710/backward/entity/BWEntityPlayer.java
+++ src/main/java/nova/wrapper/mc1710/backward/entity/BWEntityPlayer.java
@@ -1,14 +1,15 @@
 package nova.wrapper.mc1710.backward.entity;
 
+import net.minecraft.util.DamageSource;
+import nova.core.entity.components.Damageable;
 import nova.core.player.InventoryPlayer;
 import nova.core.player.Player;
 
 /**
  * A Nova to Minecraft entity wrapper
- *
  * @author Calclavia
  */
-public class BWEntityPlayer extends BWEntity implements Player {
+public class BWEntityPlayer extends BWEntity implements Player, Damageable {
 
 	public net.minecraft.entity.player.EntityPlayer entity;
 
@@ -37,5 +38,15 @@ public class BWEntityPlayer extends BWEntity implements Player {
 	@Override
 	public String getDisplayName() {
 		return entity.getDisplayName();
+	}
+
+	@Override
+	public void damage(double amount, DamageType type) {
+
+		if (type == DamageType.generic) {
+			entity.attackEntityFrom(DamageSource.generic, (float) amount);
+		}
+
+		//TODO: Apply other damage source wrappers?
 	}
 }
