b34e2a675e89f8994fac0fcd744db0ce615f2bc3
Calclavia <admin@calclavia.com>
2015-02-27 15:41:07 +0800

    Added sendPacket method to NetworkManager

--- src/main/java/nova/core/network/NetworkManager.java
+++ src/main/java/nova/core/network/NetworkManager.java
@@ -1,13 +1,9 @@
 package nova.core.network;
 
-import nova.core.block.Block;
-import nova.core.entity.Entity;
-import nova.core.item.Item;
 import nova.core.network.NetworkTarget.Side;
-import nova.core.util.exception.NovaException;
 
 /**
- * A central network manager. 
+ * A central network manager.
  * @author Calclavia
  */
 public abstract class NetworkManager {
@@ -18,8 +14,14 @@ public abstract class NetworkManager {
 	public abstract Packet newPacket();
 
 	/**
+	 * Sends a new custom packet.
+	 * @param sender The packet handler sending the packet
+	 * @param packet The packet to send
+	 */
+	public abstract void sendPacket(PacketHandler sender, Packet packet);
+
+	/**
 	 * Syncs a PacketHandler between server and client.
-	 *
 	 * @param sender {@link PacketHandler}
 	 */
 	public final void sync(PacketHandler sender) {
@@ -31,28 +33,7 @@ public abstract class NetworkManager {
 	 * @param id The packet ID
 	 * @param sender sender {@link nova.core.network.PacketHandler}
 	 */
-	public final void sync(int id, PacketHandler sender) {
-		if (sender instanceof Block) {
-			syncBlock(id, sender);
-			return;
-		} else if (sender instanceof Item) {
-			syncBlock(id, sender);
-			return;
-		} else if (sender instanceof Entity) {
-			syncBlock(id, sender);
-			return;
-		}
-
-		//TODO: Add custom packet object syncing.
-
-		throw new NovaException("Packet type not supported!");
-	}
-
-	protected abstract void syncBlock(int id, PacketHandler sender);
-
-	protected abstract void syncItem(int id, PacketHandler sender);
-
-	protected abstract void syncEntity(int id, PacketHandler sender);
+	public abstract void sync(int id, PacketHandler sender);
 
 	public final Side getSide() {
 		return isClient() ? Side.CLIENT : Side.SERVER;
