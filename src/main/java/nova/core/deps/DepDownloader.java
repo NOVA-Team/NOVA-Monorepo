eef4c4ef3a48b47e179df67c2dde6a731d8f4fff
RX14 <chris@rx14.co.uk>
2015-03-13 21:53:44 +0000

    Replace old copy with cleaner, better performing NIO copy

--- src/main/java/nova/core/deps/DepDownloader.java
+++ src/main/java/nova/core/deps/DepDownloader.java
@@ -1,38 +1,28 @@
 package nova.core.deps;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * @author Mitchellbrine
  */
 public class DepDownloader {
 
-	public static void downloadDepdency(URL versionURL, String locationToDownloadTo) {
+	public static void downloadDepdency(URL downloadURL, String locationToDownloadTo) {
 		try {
 
-			File dependencyLocation = new File(locationToDownloadTo);
+			Path dependencyLocation = Paths.get(locationToDownloadTo);
 
 			/**
-			 * This should make sure that if we already have
+			 * Do not overwrite previous files, this effectively is caching
 			 */
-			if (dependencyLocation.exists()) { return; }
+			if (dependencyLocation.toFile().exists()) { return; }
 
-			InputStream stream = versionURL.openStream();
-
-			byte[] buffer = new byte[4096];
-			int n = -1;
-
-			OutputStream output = new FileOutputStream(dependencyLocation);
-			while ((n = stream.read(buffer)) != -1) {
-				output.write(buffer, 0, n);
-			}
-			output.close();
-			stream.close();
+			Files.copy(downloadURL.openStream(), dependencyLocation, StandardCopyOption.REPLACE_EXISTING);
 
 		} catch (IOException ex) {
 			ex.printStackTrace();
