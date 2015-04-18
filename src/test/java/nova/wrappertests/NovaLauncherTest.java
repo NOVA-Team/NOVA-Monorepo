b0419a77a5fded9d610d785d3d92a6016fcf0a90
RX14 <chris@rx14.co.uk>
2015-04-18 15:45:04 +0100

    Make NovaLauncherTest abstract and incorporate DIEP tests

--- src/test/java/nova/wrappertests/NovaLauncherTest.java
+++ src/test/java/nova/wrappertests/NovaLauncherTest.java
@@ -1,18 +1,22 @@
 package nova.wrappertests;
 
 import nova.bootstrap.DependencyInjectionEntryPoint;
+import nova.core.game.Game;
 import nova.internal.NovaLauncher;
 import nova.testutils.mod.NoLoadableTestMod;
 import nova.testutils.mod.NonAnnotatedTestMod;
 import nova.testutils.mod.TestMod;
 import org.assertj.core.util.Sets;
 import org.junit.Test;
+import org.slf4j.Logger;
+import se.jbee.inject.bootstrap.Bundle;
 
+import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class NovaLauncherTest {
+public abstract class NovaLauncherTest {
 
 	Set<Class<?>> testModClasses = Sets.newLinkedHashSet(
 		TestMod.class,
@@ -20,15 +24,52 @@ public class NovaLauncherTest {
 		NoLoadableTestMod.class
 	);
 
-	@Test
-	public void testLaunchingDoesNotCrash() {
-		NovaLauncher launcher = new NovaLauncher(new DependencyInjectionEntryPoint(), testModClasses);
+	abstract List<Class<? extends Bundle>> getModules();
 
-		assertThat(launcher.getModClasses()).hasSize(1).containsValue(TestMod.class);
+	@Test
+	public void testLaunching() {
+		DependencyInjectionEntryPoint diep = new DependencyInjectionEntryPoint();
+
+		getModules().forEach(diep::install);
+
+		NovaLauncher launcher = new NovaLauncher(diep, testModClasses);
+
+		assertThat(launcher.getModClasses())
+			.hasSize(1)
+			.containsValue(TestMod.class);
 
 		launcher.generateDependencies();
 		launcher.preInit();
 		launcher.init();
 		launcher.postInit();
+	}
+
+	@Test
+	public void testResolveGame() {
+		DependencyInjectionEntryPoint diep = new DependencyInjectionEntryPoint();
+
+		getModules().forEach(diep::install);
+
+		Game game = diep.init();
+
+		assertThat(game).isNotNull();
+
+		assertThat(game.logger).isNotNull();
+		assertThat(game.clientManager).isNotNull();
+		assertThat(game.blockManager).isNotNull();
+		assertThat(game.entityManager).isNotNull();
+		assertThat(game.itemManager).isNotNull();
+		assertThat(game.fluidManager).isNotNull();
+		assertThat(game.worldManager).isNotNull();
+		assertThat(game.renderManager).isNotNull();
+		assertThat(game.recipeManager).isNotNull();
+		assertThat(game.craftingRecipeManager).isNotNull();
+		assertThat(game.itemDictionary).isNotNull();
+		assertThat(game.eventManager).isNotNull();
+		assertThat(game.networkManager).isNotNull();
+		assertThat(game.saveManager).isNotNull();
+		assertThat(game.languageManager).isNotNull();
+		assertThat(game.keyManager).isNotNull();
+		assertThat(game.nativeManager).isNotNull();
 	}
 }
