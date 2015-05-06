29adc5a907363987854365ca07fc3cb013c8e37b
Calclavia <admin@calclavia.com>
2015-05-06 16:29:39 +0800

    Added getLocal() method to LanguageManager with string replacement

--- src/main/java/nova/core/util/LanguageManager.java
+++ src/main/java/nova/core/util/LanguageManager.java
@@ -1,10 +1,21 @@
 package nova.core.util;
 
+import java.util.Map;
+
 /**
  * Manages translations from key labels to values.
- *
  * @author Calclavia
  */
 public abstract class LanguageManager {
 	public abstract String getLocal(String key);
+
+	/**
+	 * Gets the localization of a key, but applying a set of replacement strings.
+	 * @return The localized string, modified with replacements
+	 */
+	public String getLocal(String key, Map<String, String> replacements) {
+		String str = getLocal(key);
+		replacements.forEach(str::replaceAll);
+		return str;
+	}
 }
