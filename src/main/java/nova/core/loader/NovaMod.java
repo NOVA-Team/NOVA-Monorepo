7e334ad5bb5c30d5fab428ac0a0bd240ec5bc25b
Calclavia <admin@calclavia.com>
2015-05-12 18:11:20 +0800

    Added isPlugin() and description() to NovaMod

--- src/main/java/nova/core/loader/NovaMod.java
+++ src/main/java/nova/core/loader/NovaMod.java
@@ -8,8 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * An annotation placed in the mod's main loading class.
- *
+ * An annotation placed on the mod's main loading class.
  * @author Calclavia
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -17,28 +16,30 @@ import java.lang.annotation.Target;
 public @interface NovaMod {
 	/**
 	 * The unique mod identifier for this mod
-	 *
 	 * @return Mod ID
 	 */
 	String id();
 
 	/**
 	 * The user friendly name for the mod
-	 *
 	 * @return Mod name
 	 */
 	String name();
 
 	/**
 	 * The version identifier of this mod
-	 *
 	 * @return Mod Version
 	 */
 	String version();
 
 	/**
+	 * A simple description of the mod.
+	 * @return Mod description
+	 */
+	String description() default "";
+
+	/**
 	 * The version of Nova this mod is compatible with
-	 *
 	 * @return Nova version
 	 */
 	String novaVersion();
@@ -51,15 +52,19 @@ public @interface NovaMod {
 	 * "x" is the version wildcard.
 	 * Adding "f" after the version will force the dependency to be a requirement.
 	 * E.g: BuildCraft@6.1.x?
-	 *
 	 * @return The dependencies
 	 */
-	String[] dependencies() default { };
+	String[] dependencies() default {};
 
 	/**
 	 * Modules of Dependency Injection that will be added to core injector allowing provision of modules by mods.
-	 *
 	 * @return The modules
 	 */
-	Class<? extends Bundle>[] modules() default { };
+	Class<? extends Bundle>[] modules() default {};
+
+	/**
+	 * Is this NovaMod a plugin? A NOVA Plugin is usually an API and wrapper that works along NOVA to add more native features to NOVA.
+	 * @return True if the mod is a NOVA Plugin.
+	 */
+	boolean isPlugin() default false;
 }
