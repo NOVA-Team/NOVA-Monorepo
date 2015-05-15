61ba4cafd0b4a173451adc72c79d647c4d33f953
Calclavia <admin@calclavia.com>
2015-05-15 21:03:44 +0800

    Added neighborBlockChangedEvent

--- src/main/java/nova/core/event/EventManager.java
+++ src/main/java/nova/core/event/EventManager.java
@@ -4,6 +4,8 @@ import nova.core.block.Block;
 import nova.core.util.transform.Vector3i;
 import nova.core.world.World;
 
+import java.util.Optional;
+
 /**
  * General event manager that handles basic events
  * @author Calclavia
@@ -24,21 +26,45 @@ public class EventManager {
 	 */
 	public EventBus<BlockChangeEvent> blockChange = new EventBus<>();
 
-	public static class BlockChangeEvent {
+	/**
+	 * Called when a block's neighbor change.
+	 */
+	public EventBus<BlockNeighborChangeEvent> blockNeighborChange = new EventBus<>();
+
+	public static class BlockEvent extends CancelableEvent {
 		//The world
 		public final World world;
 		//The position of the block
 		public final Vector3i position;
+
+		public BlockEvent(World world, Vector3i position) {
+			this.world = world;
+			this.position = position;
+		}
+	}
+
+	public static class BlockChangeEvent extends BlockEvent {
+
 		//The block that was in this position previously
 		public final Block oldBlock;
 		//The block that was set to in this position
 		public final Block newBlock;
 
 		public BlockChangeEvent(World world, Vector3i position, Block oldBlock, Block newBlock) {
-			this.world = world;
+			super(world, position);
 			this.newBlock = newBlock;
 			this.oldBlock = oldBlock;
-			this.position = position;
+		}
+	}
+
+	@CancelableEvent.Cancelable
+	public static class BlockNeighborChangeEvent extends BlockEvent {
+		//The neighbor block
+		public final Optional<Block> neighbor;
+
+		public BlockNeighborChangeEvent(World world, Vector3i position, Optional<Block> neighbor) {
+			super(world, position);
+			this.neighbor = neighbor;
 		}
 	}
 
