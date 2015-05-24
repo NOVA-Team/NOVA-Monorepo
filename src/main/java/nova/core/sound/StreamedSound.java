ea43a76bb4e15ab25bb939b9d38ab8bf14033833
Skye M <skyem@hotmail.co.uk>
2015-05-24 11:32:09 +0100

    Added note in JavaDoc about downloadFirst in StreamedSound

--- src/main/java/nova/core/sound/StreamedSound.java
+++ src/main/java/nova/core/sound/StreamedSound.java
@@ -2,6 +2,10 @@ package nova.core.sound;
 
 /** Sound without a resource on the client **/
 public abstract class StreamedSound extends Sound {
+	/**
+	 * Makes the client wait for the download to complete before playing the sound.
+	 * If you pass it something that is endless (say a radio stream), there will be a download timeout that will stop it from continuing to download.
+	 */
 	public final boolean downloadFirst;
 
 	public StreamedSound(boolean downloadFirst, float pitchModification, float speedModification, float volumeModification) {
