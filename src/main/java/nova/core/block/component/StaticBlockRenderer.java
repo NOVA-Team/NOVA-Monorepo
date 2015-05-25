85fe9c9e6b601fc199b311f6e2a349972ae4f470
Calclavia <admin@calclavia.com>
2015-05-25 17:56:23 +0800

    StaticBlockRenderer now uses functional variables

--- src/main/java/nova/core/block/component/StaticBlockRenderer.java
+++ src/main/java/nova/core/block/component/StaticBlockRenderer.java
@@ -9,37 +9,24 @@ import nova.core.render.texture.Texture;
 import nova.core.util.Direction;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 /**
+ * A static block renderer for blocks.
  * @author Calclavia
  */
 public class StaticBlockRenderer extends StaticRenderer {
-
-	public StaticBlockRenderer(Block provider) {
-		super(provider);
-	}
-
-	/**
-	 * Called when this block is to be rendered.
-	 * @param model A {@link nova.core.render.model.Model} to use.
-	 */
-	public void renderStatic(Model model) {
-		BlockModelUtil.drawBlock(model, (Block) provider);
-
-	}
-
 	/**
 	 * Called to get the texture of this block for a certain side.
 	 * @param side The side of the block that the texture is for.
 	 * @return An optional of the texture.
 	 */
-	public Optional<Texture> getTexture(Direction side) {
-		return Optional.empty();
-	}
-
-	public boolean shouldRenderSide(Direction side) {
-		return true;
-	}
+	public Function<Direction, Optional<Texture>> texture = (dir) -> Optional.empty();
+	/**
+	 * Called when this block is to be rendered.
+	 * @param model A {@link nova.core.render.model.Model} to use.
+	 */
+	public Function<Direction, Boolean> renderSide = (dir) -> true;
 
 	/**
 	 * Gets the color of a specific face. This is called by the default block
@@ -47,7 +34,28 @@ public class StaticBlockRenderer extends StaticRenderer {
 	 * @param side - The side of the block.
 	 * @return The color
 	 */
-	public Color colorMultiplier(Direction side) {
-		return Color.white;
+	public Function<Direction, Color> colorMultiplier = (dir) -> Color.white;
+
+	public StaticBlockRenderer(Block provider) {
+		super(provider);
+	}
+
+	public StaticBlockRenderer setTexture(Function<Direction, Optional<Texture>> texture) {
+		this.texture = texture;
+		return this;
+	}
+
+	public StaticBlockRenderer setRenderSide(Function<Direction, Boolean> renderSide) {
+		this.renderSide = renderSide;
+		return this;
+	}
+
+	public StaticBlockRenderer setColorMultiplier(Function<Direction, Color> colorMultiplier) {
+		this.colorMultiplier = colorMultiplier;
+		return this;
+	}
+
+	public void renderStatic(Model model) {
+		BlockModelUtil.drawBlock(model, (Block) provider);
 	}
 }
