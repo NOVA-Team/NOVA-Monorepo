b7f39fc7e6781d8a9e761c30b9a87fe9ca166f54
Calclavia <admin@calclavia.com>
2015-05-26 15:00:26 +0800

    Minor component system bug fixes

--- src/main/java/nova/core/game/ClientManager.java
+++ src/main/java/nova/core/game/ClientManager.java
@@ -1,6 +1,6 @@
 package nova.core.game;
 
-import nova.core.entity.component.Player;
+import nova.core.entity.Entity;
 
 /**
  * The ClientManager to access client-side instances.
@@ -9,7 +9,7 @@ import nova.core.entity.component.Player;
 public abstract class ClientManager {
 
 	/**
-	 * @return Gets the current player on this running client
+	 * @return Gets the current player on this running client. An entity with the Player component.
 	 */
-	public abstract Player getPlayer();
+	public abstract Entity getPlayer();
 }
