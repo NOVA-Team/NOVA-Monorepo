8fa3cb82dd2a6d78d4d3ee78d187f09d1b4f8659
Calclavia <admin@calclavia.com>
2015-05-27 22:46:02 +0800

    Fixed BWWorld not implementing playSoundAtPosition

--- src/main/java/nova/core/sound/Sound.java
+++ src/main/java/nova/core/sound/Sound.java
@@ -11,22 +11,22 @@ public abstract class Sound implements Identifiable {
 	 * Changes the pitch of the sound.
 	 * This does not change the speed of the sound so it can it be used to compensate for the speed of the sound changing to pitch.
 	 */
-	public final float pitchModification;
+	public final float pitch;
 	/**
 	 * Changes the speed of the sound.
 	 * This also changes the pitch of the sound, and so the pitch change should be compensated with pitchModification.
 	 */
-	public final float speedModification;
+	public final float speed;
 	/**
 	 * Changes the volume of the sound.
 	 * Default is 1 for no modification.
 	 */
-	public final float volumeModification;
+	public final float volume;
 
-	public Sound(float pitchModification, float speedModification, float volumeModification) {
-		this.pitchModification = pitchModification;
-		this.speedModification = speedModification;
-		this.volumeModification = volumeModification;
+	public Sound(float pitch, float speed, float volume) {
+		this.pitch = pitch;
+		this.speed = speed;
+		this.volume = volume;
 	}
 	public Sound() {
 		this(1, 1, 1);
