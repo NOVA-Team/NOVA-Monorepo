bb1c294a45cb0be7cf31ca183e25cd45557c3d04
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-05-29 09:06:59 +0200

    Added getters to Game class.

--- src/main/java/nova/core/game/Game.java
+++ src/main/java/nova/core/game/Game.java
@@ -22,7 +22,6 @@ import nova.internal.tick.UpdateTicker;
 import org.slf4j.Logger;
 
 public class Game {
-
 	public static Game instance;
 
 	public final Logger logger;
@@ -108,5 +107,95 @@ public class Game {
 		this.guiFactory = guiFactory;
 
 		logger.info("Game instance created.");
+	}
+
+	public static Game getInstance() {
+		return instance;
+	}
+
+	public Logger getLogger() {
+		return logger;
+	}
+
+	public ClientManager getClientManager() {
+		return clientManager;
+	}
+
+	public BlockManager getBlockManager() {
+		return blockManager;
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public ItemManager getItemManager() {
+		return itemManager;
+	}
+
+	public FluidManager getFluidManager() {
+		return fluidManager;
+	}
+
+	public WorldManager getWorldManager() {
+		return worldManager;
+	}
+
+	public RenderManager getRenderManager() {
+		return renderManager;
+	}
+
+	public RecipeManager getRecipeManager() {
+		return recipeManager;
+	}
+
+	public CraftingRecipeManager getCraftingRecipeManager() {
+		return craftingRecipeManager;
+	}
+
+	public ItemDictionary getItemDictionary() {
+		return itemDictionary;
+	}
+
+	public EventManager getEventManager() {
+		return eventManager;
+	}
+
+	public NetworkManager getNetworkManager() {
+		return networkManager;
+	}
+
+	public SaveManager getSaveManager() {
+		return saveManager;
+	}
+
+	public LanguageManager getLanguageManager() {
+		return languageManager;
+	}
+
+	public KeyManager getKeyManager() {
+		return keyManager;
+	}
+
+	public ComponentManager getComponentManager() {
+		return componentManager;
+	}
+
+	public NativeManager getNativeManager() {
+		return nativeManager;
+	}
+
+	/**
+	 * The synchronized ticker that uses the same thread as the game.
+	 */
+	public UpdateTicker.SynchronizedTicker getSyncTicker() {
+		return syncTicker;
+	}
+
+	/**
+	 * The thread ticker that runs on NOVA's thread.
+	 */
+	public final UpdateTicker.ThreadTicker getThreadTicker() {
+		return threadTicker;
 	}
 }
