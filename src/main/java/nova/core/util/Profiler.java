6311aaa1dc633acefd81631231ff0424ebaa6c90
Calclavia <admin@calclavia.com>
2015-05-31 13:26:04 +0800

    Added Profiler

---
+++ src/main/java/nova/core/util/Profiler.java
@@ -0,0 +1,50 @@
+package nova.core.util;
+
+import nova.core.game.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple stop watch profiler.
+ * @author Calclavia
+ */
+public class Profiler {
+
+	public final String name;
+	private final List<Long> lapped = new ArrayList<>();
+	private Long time;
+
+	public Profiler(String name) {
+		this.name = name;
+	}
+
+	public Profiler start() {
+		time = System.currentTimeMillis();
+		return this;
+	}
+
+	public Profiler end() {
+		Game.instance().logger().info(toString());
+		time = System.currentTimeMillis();
+		return this;
+	}
+
+	public long elapsed() {
+		return System.currentTimeMillis() - time;
+	}
+
+	public double average() {
+		return lapped.stream().mapToDouble(value -> value / 1000d).sum() / lapped.size();
+	}
+
+	public Profiler lap() {
+		lapped.add(elapsed());
+		return start();
+	}
+
+	@Override
+	public String toString() {
+		return name + " took " + ((System.currentTimeMillis() - time) / 1000d) + " seconds";
+	}
+}
