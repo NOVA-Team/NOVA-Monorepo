0a6a135d7cb2eb7274b5f7848b4358d899d4d8f9
Calclavia <admin@calclavia.com>
2015-06-02 00:33:47 +0800

    Added Connectable component

---
+++ src/main/java/nova/core/block/component/Connectable.java
@@ -0,0 +1,37 @@
+package nova.core.block.component;
+
+import nova.core.component.Component;
+import nova.core.event.Event;
+import nova.core.event.EventBus;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * A component that defines a connection with another.
+ * @author Calclavia
+ * @tparam C The connector type
+ */
+public class Connectable<C> extends Component {
+
+	public final EventBus<Event> connectEvent = new EventBus<>();
+
+	/**
+	 * Can this connectable component connect to another component?
+	 */
+	public Function<C, Boolean> canConnect = C -> true;
+
+	public Supplier<Set<C>> connections = () -> Collections.emptySet();
+
+	public Connectable setCanConnect(Function<C, Boolean> canConnect) {
+		this.canConnect = canConnect;
+		return this;
+	}
+
+	public Connectable setConnections(Supplier<Set<C>> connections) {
+		this.connections = connections;
+		return this;
+	}
+}
