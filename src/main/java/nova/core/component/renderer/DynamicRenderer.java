5131c42e0449d97229e05345ea96035f1826c004
Calclavia <admin@calclavia.com>
2015-06-03 11:37:51 +0800

    DynamicRenderer now does not requires constructor parameter

--- src/main/java/nova/core/component/renderer/DynamicRenderer.java
+++ src/main/java/nova/core/component/renderer/DynamicRenderer.java
@@ -1,15 +1,8 @@
 package nova.core.component.renderer;
 
-import nova.core.component.ComponentProvider;
-
 /**
  * This interface signals that a block needs dynamic rendering.
  */
 public class DynamicRenderer extends Renderer {
 
-	public final ComponentProvider provider;
-
-	public DynamicRenderer(ComponentProvider provider) {
-		this.provider = provider;
-	}
 }
