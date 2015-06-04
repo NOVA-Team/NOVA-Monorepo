fc2e26c5f5a997dd2da0551cc35e2150e26386cf
Calclavia <admin@calclavia.com>
2015-06-04 15:36:18 +0800

    Implemented UniqueIdentifiable on Entity

--- src/main/java/nova/wrapper/mc1710/wrapper/entity/forward/MCEntityWrapper.java
+++ src/main/java/nova/wrapper/mc1710/wrapper/entity/forward/MCEntityWrapper.java
@@ -1,16 +1,22 @@
 package nova.wrapper.mc1710.wrapper.entity.forward;
 
 import net.minecraft.entity.Entity;
+import nova.core.util.UniqueIdentifiable;
 import nova.internal.dummy.Wrapper;
 
 /**
  * @author Calclavia
  */
-public class MCEntityWrapper extends Wrapper {
+public class MCEntityWrapper extends Wrapper implements UniqueIdentifiable {
 
 	public final net.minecraft.entity.Entity wrapper;
 
 	public MCEntityWrapper(Entity wrapper) {
 		this.wrapper = wrapper;
+	}
+
+	@Override
+	public String getUniqueID() {
+		return wrapper.getUniqueID().toString();
 	}
 }
