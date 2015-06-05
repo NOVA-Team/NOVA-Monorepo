e8fb92beceed8139f25d1b573dbcf51e15024045
Calclavia <admin@calclavia.com>
2015-06-05 14:24:42 +0800

    WorldManager is now capable of providing reference to worlds

--- src/main/java/nova/core/world/WorldManager.java
+++ src/main/java/nova/core/world/WorldManager.java
@@ -1,12 +1,43 @@
 package nova.core.world;
 
+import nova.core.game.Game;
+import nova.core.util.Factory;
+import nova.core.util.Manager;
 import nova.core.util.Registry;
 
-public class WorldManager {
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
 
-	public final Registry<World> worldRegistry;
+public class WorldManager extends Manager<World, Factory<World>> {
 
-	private WorldManager(Registry<World> worldRegistry) {
-		this.worldRegistry = worldRegistry;
+	/**
+	 * The set of worlds that currently exist
+	 */
+	public final Set<World> clientWorlds = new HashSet<>();
+	public final Set<World> serverWorlds = new HashSet<>();
+
+	public WorldManager(Registry<Factory<World>> registry) {
+		super(registry);
+	}
+
+	@Override
+	public Factory<World> register(Function<Object[], World> constructor) {
+		return register(new Factory<>(constructor));
+	}
+
+	public Set<World> sidedWorlds() {
+		if (Game.network().isClient()) {
+			return clientWorlds;
+		}
+		return serverWorlds;
+	}
+
+	public Optional<World> findWorld(String id) {
+		return sidedWorlds()
+			.stream()
+			.filter(world -> id.equals(world.getID()))
+			.findFirst();
 	}
 }
