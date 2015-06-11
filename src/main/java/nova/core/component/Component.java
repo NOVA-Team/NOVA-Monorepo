bf1e7db603cd30ef9b96e6b79c2757080d3cc5f9
Calclavia <admin@calclavia.com>
2015-06-11 11:06:35 +0800

    Added events to Component

--- src/main/java/nova/core/component/Component.java
+++ src/main/java/nova/core/component/Component.java
@@ -1,5 +1,9 @@
 package nova.core.component;
 
+import nova.core.event.Event;
+import nova.core.event.EventBus;
+import nova.core.event.EventListener;
+import nova.core.event.EventListenerHandle;
 import nova.core.util.Identifiable;
 
 /**
@@ -8,6 +12,16 @@ import nova.core.util.Identifiable;
  * @author Calclavia
  */
 public abstract class Component implements Identifiable {
+
+	public final EventBus<Event> events = new EventBus<>();
+
+	public <EVENT extends Event> EventListenerHandle<Event> on(EventListener<EVENT> listener, Class<EVENT> clazz) {
+		return events.add(listener, clazz);
+	}
+
+	public <EVENT extends Event> EventListenerHandle<Event> on(EventListener<EVENT> listener, Class<EVENT> clazz, int priority) {
+		return events.add(listener, clazz, priority);
+	}
 
 	@Override
 	public String getID() {
