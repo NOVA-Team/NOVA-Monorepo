04b13349c22e023b82a53935917089a44209a537
Calclavia <admin@calclavia.com>
2015-06-13 10:56:48 +0800

    Some color fixes

--- src/main/java/nova/core/render/Color.java
+++ src/main/java/nova/core/render/Color.java
@@ -163,11 +163,7 @@ public class Color {
 	}
 
 	public int rgba() {
-		//TODO: This is bugged.
-		int alpha = value & 0xFF000000;
-		int rgba = value << 8;
-		rgba |= alpha >> 24;
-		return rgba;
+		return (red() << 24) | (green() << 16) | (blue() << 8) | (alpha());
 	}
 
 	public int red() {
