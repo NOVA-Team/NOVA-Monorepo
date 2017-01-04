a1fec79d3219d0dca1660de27bf821615d5a5b82
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-01-04 09:52:25 +0100

    Make EnumSelector an Iterable + Additional Methods
    Also updated test to contain support for new methods.

--- src/test/java/nova/core/util/EnumSelectorTest.java
+++ src/test/java/nova/core/util/EnumSelectorTest.java
@@ -20,6 +20,8 @@ public class EnumSelectorTest {
 
 	EnumSelector <EnumExample> enumSelectorExample1;
 	EnumSelector <EnumExample> enumSelectorExample2;
+	EnumSelector <EnumExample> enumSelectorExample3;
+	EnumSelector <EnumExample> enumSelectorExample4;
 
 	public EnumSelectorTest() {
 	}
@@ -31,6 +33,9 @@ public class EnumSelectorTest {
 
 		enumSelectorExample2 = EnumSelector.of(EnumExample.class).allowAll()
 				.apart(EnumExample.EXAMPLE_24).apart(EnumExample.EXAMPLE_42).lock();
+
+		enumSelectorExample3 = EnumSelector.of(EnumExample.class).allowAll().lock();
+		enumSelectorExample4 = EnumSelector.of(EnumExample.class).blockAll().lock();
     }
 
 	@Test
@@ -127,6 +132,54 @@ public class EnumSelectorTest {
 	public void test2Allows_EXAMPLE_64() {
 		boolean result = enumSelectorExample2.allows(EnumExample.EXAMPLE_64);
 		assertThat(result).isEqualTo(true);
+	}
+
+	@Test
+	public void test3AllowsAll() {
+		boolean result = enumSelectorExample3.allowsAll();
+		assertThat(result).isEqualTo(true);
+	}
+
+	@Test
+	public void test3NotBlocksAll() {
+		boolean result = enumSelectorExample3.blocksAll();
+		assertThat(result).isEqualTo(false);
+	}
+
+	@Test
+	public void test4NotAllowsAll() {
+		boolean result = enumSelectorExample4.allowsAll();
+		assertThat(result).isEqualTo(false);
+	}
+
+	@Test
+	public void test4BlocksAll() {
+		boolean result = enumSelectorExample4.blocksAll();
+		assertThat(result).isEqualTo(true);
+	}
+
+	@Test
+	public void test1StreamSize() {
+		long result = enumSelectorExample1.stream().count();
+		assertThat(result).isEqualTo(2);
+	}
+
+	@Test
+	public void test2StreamSize() {
+		long result = enumSelectorExample2.stream().count();
+		assertThat(result).isEqualTo(EnumExample.values().length - 2);
+	}
+
+	@Test
+	public void test3StreamSize() {
+		long result = enumSelectorExample3.stream().count();
+		assertThat(result).isEqualTo(EnumExample.values().length);
+	}
+
+	@Test
+	public void test4StreamSize() {
+		long result = enumSelectorExample4.stream().count();
+		assertThat(result).isEqualTo(0);
 	}
 
 	public static enum EnumExample {
